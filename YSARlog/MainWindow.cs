0f4bd3a87f3d61f62e1751862a1aa797d531e796
r.d.cohen <r.d.cohen@student.saintkentigern.com>
2021-03-23 18:10:54 +1300

    v2 - added table but not working

---
+++ YSARlog/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace YSARlog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var data = new Log { team = "team", time = "time", message = "message" };
+
+            DataGridSar.Items.Add(data);
+        }
+    }
+
+    public class Log
+    {
+        public string team { get; set; }
+        public string time { get; set; }
+        public string message { get; set; }
+    } 
+
+}
