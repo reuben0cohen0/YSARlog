95f933efecf186f1418914d5ce3c12616d5d6121
r.d.cohen <r.d.cohen@student.saintkentigern.com>
2021-04-08 11:38:42 +1200

    attermpting streamwriter to save log - not working

--- YSARlog/MainWindow.cs
+++ YSARlog/MainWindow.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.IO;
+using System.Text;
 
 
 namespace YSARlog
@@ -14,22 +16,47 @@ namespace YSARlog
         public MainWindow()
         {
             InitializeComponent();
+            List<datas> users = new List<datas>();
+            users.Add(new datas() { Team = "", Time = "" + DateTime.Now + "", Message = "" });
+
+            string fileName = "log.txt";
+            string textToAdd = "";
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.Write(textToAdd);
+            }
+
         }//end mainwindow
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             List<datas> users = new List<datas>();
-            users.Add(new datas() { team = "", time = "", message = "" });
+            users.Add(new datas() { Team = "", Time = ""+ DateTime.Now +"", Message = "" });
 
             DataGridSar.ItemsSource = users;
+
         }//end button click
+
+        private void Button_Click_2(object sender, EventArgs e)
+        {
+            string fileName = @"C:\Users\r.d.cohen\source\repos\YSARlog\YSARlog\log.txt";
+            string textToAdd = ($"Log: {Team}");
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.Write(textToAdd);
+            }
+        }
+
 
         private class datas
         {
-            public string team { get; set; }
-            public string time { get; set; }
-            public string message { get; set; }
+            public string Team { get; set; }
+            public string Time { get; set; }
+            public string Message { get; set; }
         }
+
     }//end window
        
 
