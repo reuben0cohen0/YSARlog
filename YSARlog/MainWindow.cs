585111a834e5e6d9e35946f87a1bd1c6b7b78416
r.d.cohen <r.d.cohen@student.saintkentigern.com>
2021-04-13 12:30:02 +1200

    streamwriter working - help box added

--- YSARlog/MainWindow.cs
+++ YSARlog/MainWindow.cs
@@ -6,6 +6,11 @@ using System.Windows.Forms;
 using System.IO;
 using System.Text;
 using System.Reflection;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
 
 
 namespace YSARlog
@@ -30,7 +35,7 @@ namespace YSARlog
         public void Button_Click_1(object sender, RoutedEventArgs e)
         {
             List<Datas> users = new List<Datas>();
-            Datas newUser = new Datas() { Team = "", Time = "" + DateTime.Now + "", Message = "" };
+            Datas newUser = new Datas() { Time = "" + DateTime.Now + "", Team = "", Message = "" };
             users.Add(newUser);
 
 
@@ -44,22 +49,34 @@ namespace YSARlog
             string textToAdd = "test";
 
             IEnumerator enumerator = DataGridSar.ItemsSource.GetEnumerator();
-            enumerator.MoveNext();
-            Datas item = (Datas) enumerator.Current;
-            textToAdd = item.Team;
-            /*
+            //enumerator.MoveNext();
+            //Datas item = (Datas) enumerator.Current;
+            //textToAdd = item.Team;
+            using (StreamWriter writer = new StreamWriter(fileName))
             while (enumerator.MoveNext())
             {
-                object item = enumerator.Current;
-                textToAdd = $"Log: {item[0]}";
+                Datas item = (Datas) enumerator.Current;
+                textToAdd = $"Log: {item.Time}, {item.Team}, {item.Message}";
+                
+                writer.WriteLine(textToAdd);
             }
-           */
+           
 
-            using (StreamWriter writer = new StreamWriter(fileName))
-            {
-                writer.Write(textToAdd);
-            }
+            
+            
         }
+
+        private void Button_Click_3(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Welcome to the YSARLog Program" + Environment.NewLine +
+                "To start a new log, click File -> New" + Environment.NewLine +
+                "To save the log at the end of the exercsie, click File -> Save" + Environment.NewLine +
+               "Please note that clicking save will overwrite the log.txt file. Make sure to make a copy of your previous ones or rename them when you have finished" + Environment.NewLine);
+      
+
+            
+        }
+
         public class Datas : IEnumerable
         {
             public string Team { get; set; }
